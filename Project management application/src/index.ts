98e696fd66178635e5d8c5775ca67b1a1fb9bd6e
pashasha <smogida1337@gmail.com>
2022-11-14 13:25:48 +0300

    ff

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://Clearenough:<1111>@cluster0.lcbluem.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://Clearenough:<JliUnLDMTUulozWS>@cluster0.lcbluem.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
